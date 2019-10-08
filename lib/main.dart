8226193b1fad9f40079781ca07d66b43fc6e2a44
Mohammad Rahmani <rto1680@gmail.com>
2019-10-08 22:10:52 +0430

    Make main page

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,168 @@
+import 'package:day15_shoes/Animation/FadeAnimation.dart';
+import 'package:day15_shoes/Shoes.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage()
+  )
+);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: null,
+        title: Text("Shoes", style: TextStyle(color: Colors.black, fontSize: 25),),
+        brightness: Brightness.light,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none, color: Colors.black,),
+          ),
+          IconButton(
+            icon: Icon(Icons.shopping_cart, color: Colors.black,),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 40,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 2.2/1,
+                      child: FadeAnimation(1, Container(
+                        margin: EdgeInsets.only(right: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.grey[200],
+                          borderRadius: BorderRadius.circular(20)
+                        ),
+                        child: Center(
+                          child: Text("All", style: TextStyle(fontSize: 20),),
+                        ),
+                      )),
+                    ),
+                    AspectRatio(
+                      aspectRatio: 2.2/1,
+                      child: FadeAnimation(1.1, Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Center(
+                          child: Text("Sneakers", style: TextStyle(fontSize: 17),),
+                        ),
+                      )),
+                    ),
+                    AspectRatio(
+                      aspectRatio: 2.2/1,
+                      child: FadeAnimation(1.2, Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Center(
+                          child: Text("Football", style: TextStyle(fontSize: 17),),
+                        ),
+                      )),
+                    ),
+                    AspectRatio(
+                      aspectRatio: 2.2/1,
+                      child: FadeAnimation(1.3, Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Center(
+                          child: Text("Soccer", style: TextStyle(fontSize: 17),),
+                        ),
+                      )),
+                    ),
+                    AspectRatio(
+                      aspectRatio: 2.2/1,
+                      child: FadeAnimation(1.4, Container(
+                        margin: EdgeInsets.only(right: 10),
+                        child: Center(
+                          child: Text("Golf", style: TextStyle(fontSize: 17),),
+                        ),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+              FadeAnimation(1.5, makeItem(image: 'assets/images/one.jpg', tag: 'red', context: context)),
+              FadeAnimation(1.6, makeItem(image: 'assets/images/two.jpg', tag: 'blue', context: context)),
+              FadeAnimation(1.7, makeItem(image: 'assets/images/three.jpg', tag: 'white', context: context)),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget makeItem({image, tag, context}) {
+    return Hero(
+      tag: tag,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => Shoes(image: image,)));
+        },
+        child: Container(
+          height: 250,
+          width: double.infinity,
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.only(bottom: 20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+              image: AssetImage(image),
+              fit: BoxFit.cover
+            ),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey[400],
+                blurRadius: 10,
+                offset: Offset(0, 10)
+              )
+            ]
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        FadeAnimation(1, Text("Sneakers", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),
+                        SizedBox(height: 10,),
+                        FadeAnimation(1.1, Text("Nike", style: TextStyle(color: Colors.white, fontSize: 20),)),
+
+                      ],
+                    ),
+                  ),
+                  FadeAnimation(1.2, Container(
+                    width: 35,
+                    height: 35,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white
+                    ),
+                    child: Center(
+                      child: Icon(Icons.favorite_border, size: 20,),
+                    ),
+                  ))
+                ],
+              ),
+              FadeAnimation(1.2, Text("100\$", style: TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),)),              
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
